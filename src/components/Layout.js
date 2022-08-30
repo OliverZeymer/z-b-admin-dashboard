69430e45de2375e76f85524212de430f2b5b2be2
benamager <benaprivat@gmail.com>
2022-08-30 11:31:06 +0200

    Finished notification widget

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "./Navbar"
+import NotificationWidget from "./NotificationWidget"
+
 const Layout = () => {
   return (
-    <div className="mx-auto w-[90%]">
+    <div className="mx-auto w-[90%] relative">
       <Navbar />
+      <NotificationWidget />
       <main>
         <Outlet />
       </main>
