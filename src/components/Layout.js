adb605aa28acadf1307890d20c6c13d927c6a4aa
OtakuBoy420 <U28282@rts-365.dk>
2022-08-30 13:12:37 +0200

    fix navbar issues

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,18 +1,19 @@
-
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import NotificationWidget from "./NotificationWidget"
+import NotificationWidget from "./NotificationWidget";
 const Layout = () => {
   return (
-    <>
+    <div className="sm:flex">
       <Navbar />
       <NotificationWidget />
       <div className="mx-auto w-[90%] relative">
         <main>
           <Outlet />
+
+          <h1 className="text-primary-text">lort</h1>
         </main>
       </div>
-    </>
+    </div>
   );
 };
-
+export default Layout;
