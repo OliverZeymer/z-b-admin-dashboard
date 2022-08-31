43b4df0c7a9fe3176ccd9abe3512a41d052e2181
benamager <benaprivat@gmail.com>
2022-08-31 12:59:37 +0200

    Init

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,5 +1,21 @@
-const Product = () => {
-  return <div></div>;
-};
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
 
-export default Product;
+const Product = () => {
+  const style = css`
+    grid-template-columns: 1fr 2fr 1fr;
+  `
+
+  return (
+    <section>
+      <h1>Editing product - Apple airdots (#AH123)</h1>
+      <div css={style} className="grid">
+        <p>hej</p>
+        <p>hej</p>
+        <p>hej</p>
+      </div>
+    </section>
+  )
+}
+
+export default Product
