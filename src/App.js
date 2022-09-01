319d4eaddfe34292f4b2edd2f6977178cc5cb2d6
OtakuBoy420 <U28282@rts-365.dk>
2022-09-01 20:09:33 +0200

    search init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "./pages/Login"
-import Orders from "./pages/Orders"
-import Order from "./pages/Order"
-import Customers from "./pages/Customers"
-import Customer from "./pages/Customer"
-import Products from "./pages/Products"
-import Product from "./pages/Product"
-import tokenContext from "./contexts/tokenContext"
-import notificationContext from "./contexts/notificationContext"
-import Layout from "./components/Layout"
-import { useState } from "react"
-import themeContext from "./contexts/themeContext"
-import { setColors } from "./functions/setColors"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import Order from "./pages/Order";
+import Customers from "./pages/Customers";
+import Customer from "./pages/Customer";
+import Products from "./pages/Products";
+import Product from "./pages/Product";
+import tokenContext from "./contexts/tokenContext";
+import notificationContext from "./contexts/notificationContext";
+import Layout from "./components/Layout";
+import { useState } from "react";
+import themeContext from "./contexts/themeContext";
+import { setColors } from "./functions/setColors";
+import searchContext from "./contexts/searchContext";
 function App() {
-  const [token, setToken] = useState("sfdcnhlisgnchg")
-
+  const [token, setToken] = useState("sfdcnhlisgnchg");
+  const [notification, setNotification] = useState([]);
+  const [search, setSearch] = useState("");
   /*-------------THEME LOCAL STORAGE---------- */
-  const themeLS = JSON.parse(window.localStorage.getItem("theme"))
-  const [savedTheme] = useState(themeLS)
-  const [theme, setTheme] = useState(savedTheme || "light")
-  const [notification, setNotification] = useState([])
+  const themeLS = JSON.parse(window.localStorage.getItem("theme"));
+  const [savedTheme] = useState(themeLS);
+  const [theme, setTheme] = useState(savedTheme || "light");
 
   setColors(
     theme === "dark" ? "var(--darkmode-color)" : "",
@@ -27,33 +28,35 @@ function App() {
     theme === "dark" ? "var(--darkmode-background)" : "",
     theme === "dark" ? "var(--darkmode-theme)" : "",
     theme === "dark" ? "var(--darkmode-input)" : ""
-  )
+  );
   /*------------------------------------------*/
 
   return (
     <notificationContext.Provider value={{ notification, setNotification }}>
       <tokenContext.Provider value={{ token, setToken }}>
         <themeContext.Provider value={{ theme, setTheme }}>
-          <BrowserRouter>
-            <Routes>
-              {token ? (
-                <Route path="/" default element={<Layout />}>
-                  <Route path="/orders" element={<Orders />} />
-                  <Route path="/order/:id" element={<Order />} />
-                  <Route path="/customers" element={<Customers />} />
-                  <Route path="/customer/:id" element={<Customer />} />
-                  <Route path="/products" element={<Products />} />
-                  <Route path="/product/:id" element={<Product />} />
-                </Route>
-              ) : (
-                <Route path="/" default element={<Login />} />
-              )}
-            </Routes>
-          </BrowserRouter>
+          <searchContext.Provider value={{ search, setSearch }}>
+            <BrowserRouter>
+              <Routes>
+                {token ? (
+                  <Route path="/" default element={<Layout />}>
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/order/:id" element={<Order />} />
+                    <Route path="/customers" element={<Customers />} />
+                    <Route path="/customer/:id" element={<Customer />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/product/:id" element={<Product />} />
+                  </Route>
+                ) : (
+                  <Route path="/" default element={<Login />} />
+                )}
+              </Routes>
+            </BrowserRouter>
+          </searchContext.Provider>
         </themeContext.Provider>
       </tokenContext.Provider>
     </notificationContext.Provider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,12 +1,10 @@
 import List from "../components/List";
-import Search from "../components/Search";
 const Products = () => {
   const list = {
     headings: ["Product ID", "Name", "Price", "Stock", "Amount sold"],
   };
   return (
     <div>
-      <Search placeholder="Enter Product ID or Product Name" />
       <List type="products" data={list} />
     </div>
   );
